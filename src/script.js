1c7328a85540c5cf26059a98f3d10cbaaa72760b
Giovanni Gonçalves <giovanniglirio@gmail.com>
2023-12-11 20:47:08 -0300

    Calculadora DoDev Concluído

--- src/script.js
+++ src/script.js
@@ -1,8 +1,49 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(num1, num2, operador) {
+  // Convertendo as strings para números
+  const numero1 = parseFloat(num1);
+  const numero2 = parseFloat(num2);
 
+  // Variável para armazenar o resultado
+  let resultado;
+
+  // Realizando a operação com base no operador
+  switch (operador) {
+      case '+':
+          resultado = numero1 + numero2;
+          break;
+      case '-':
+          resultado = numero1 - numero2;
+          break;
+      case '*':
+          resultado = numero1 * numero2;
+          break;
+      case '/':
+          if (numero2 !== 0) {
+              resultado = numero1 / numero2;
+          } else {
+              return '/0 não existe';
+          }
+          break;
+      case 'e':
+          resultado = 1;
+          for (let contador = 0; i < numero2; contador++) {
+              resultado *= numero1;
+          }
+          break;
+      default:
+          return 'Operador inválido';
+  }
+
+     // Substituindo o resultado por "ERRO" se for undefined ou maior que 1.000.000
+     if (resultado === undefined || resultado > 1000000) {
+      resultado = 'ERRO';
+  }
+
+  return resultado;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
